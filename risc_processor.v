b21801c6710ba005088a5b03d6bf661b19fcef59
Toth <dtoth1@ad.brown.edu>
2022-04-18 17:25:09 -0400

    datamemory black box created using megawizard

--- risc_processor.v
+++ risc_processor.v
@@ -1,2 +1,17 @@
-module risc_processor(clk);
+module risc_processor(clk50);
+	
+	wire clk80
+	
+	
+	always @(*): begin
+		//combinational logic blocks
+	end
+	
+	always@(posedge clk80): begin
+		//x = next_x
+	end
+	
+	always@(negedge clk80): begin
+		//x = next_x
+	end
 endmodule
